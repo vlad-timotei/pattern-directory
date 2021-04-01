862e461578ae5040440f533f43cdce0cc282d000
dufresnesteven <steve.dufresne@automattic.com>
2021-04-01 10:29:36 +0900

    Update the name of the component to category.

--- public_html/wp-content/themes/pattern-directory/src/index.js
+++ public_html/wp-content/themes/pattern-directory/src/index.js
@@ -7,6 +7,7 @@ import { render } from '@wordpress/element';
  * Internal dependencies
  */
 import PatternPreview from './components/pattern-preview';
+import CategoryContextBar from './components/category-context-bar';
 
 // Load the preview into any awaiting preview container.
 const previewContainers = document.querySelectorAll( '.pattern-preview__container' );
@@ -15,7 +16,12 @@ for ( let i = 0; i < previewContainers.length; i++ ) {
 	const blockContent = JSON.parse( decodeURIComponent( container.innerText ) );
 	// Use `wp.blocks.parse` to convert HTML to block objects (for use in editor), if needed.
 
-	render( <PatternPreview blockContent={ blockContent } />, container, () => {
+	render( <>
+    {/* <PatternPreview blockContent={ blockContent } /> */}
+    <CategoryContextBar actionsTitle="Something" actions={ [ { label: 'Url', href: '#url'},{ label: 'Thin', href: '#nothing'} ]}>
+        This is the contexzt
+    </CategoryContextBar>
+    </>, container, () => {
 		// This callback is called after the render to unhide the container.
 		container.hidden = false;
 	} );
